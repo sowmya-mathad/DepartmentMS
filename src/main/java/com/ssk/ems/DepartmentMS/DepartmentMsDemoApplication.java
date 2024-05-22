632ab4227a34270cda707303a8168a42db0386f4
sowmyaMac <sgm12624@gmail.com>
2024-05-22 10:09:05 -0400

    initial commit

---
+++ src/main/java/com/ssk/ems/DepartmentMS/DepartmentMsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ssk.ems.DepartmentMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentMsDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DepartmentMsDemoApplication.class, args);
+	}
+
+}
